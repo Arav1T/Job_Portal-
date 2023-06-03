05d1eb382cf640dd3ffa96abe359def3e158ee53
Arav1T <aravtiwari127@gmail.com>
2023-06-04 00:11:15 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App h-11 bg-slate-600">
+      knnikn
+    </div>
+  );
+}
+
+export default App;
